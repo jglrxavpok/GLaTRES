b71172a7eb05e357c887de7462e07059d99f9e25
jglrxavpok <jglrxavpok@gmail.com>
2015-03-20 20:40:50 +0100

    The SQLStockage now creates the coreData table when needed

--- Stockage/src/main/java/org/glatres/stockage/SQLStockage.java
+++ Stockage/src/main/java/org/glatres/stockage/SQLStockage.java
@@ -17,21 +17,45 @@ public class SQLStockage extends StockageSystem {
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
             connection = DriverManager.getConnection(url);
+            String createStat = "create table coreData (\n";
+            createStat += " ID          INTEGER NOT NULL\n";
+            createStat += "PRIMARY KEY GENERATED ALWAYS AS IDENTITY\n";
+            createStat += "(START WITH 0, INCREMENT BY 1),\n";
+            createStat += "BOT_NAME VARCHAR(30)\n";
+            createStat += ")";
+            try {
+                exec(createStat);
+            } catch (SQLException e) {
+                if (tableAlreadyExists(e)) {
+                    return this; // That's OK
+                }
+                throw e;
+            }
+
+            System.out.println("Created table coreData");
         } catch (Exception e) {
             e.printStackTrace();
         }
         return this;
     }
 
-    private ResultSet exec(String query) {
+    private boolean tableAlreadyExists(SQLException e) {
+        return "X0Y32".equals(e.getSQLState());
+    }
+
+    private boolean exec(String query) throws SQLException {
+        boolean result = false;
+        Statement stat = connection.createStatement();
+        result = stat.execute(query);
+        stat.close();
+        return result;
+    }
+
+    private ResultSet execQuery(String query) throws SQLException {
         ResultSet result = null;
-        try {
-            Statement stat = connection.createStatement();
-            result = stat.executeQuery(query);
-            stat.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        Statement stat = connection.createStatement();
+        result = stat.executeQuery(query);
+        stat.close();
 
         return result;
     }
